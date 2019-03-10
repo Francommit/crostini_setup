120bef61dafc1dd9f7d8d8faa226775b8047417a
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-03-10 15:42:08 +1100

    Added DotnetCore 2.2 installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -51,8 +51,15 @@ sudo apt-get install -f
 # Docker installation
 curl -L https://gist.githubusercontent.com/upbeta01/3b968320b3a579c326ab6cd2a195b10d/raw/196eb33a6e4b5ef9abae98d9e221ebd62a61fd65/install-docker-deb9.sh | bash
 
-# TO-DO: Dotnet Core
-
+# Dotnet Core 2.2
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/9/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.2
 
 #
 # Configurations
