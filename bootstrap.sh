fd14b72e62ee2f2bc25517ac141f2e00717ac724
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-04-30 10:23:33 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -70,8 +70,8 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak install flathub org.remmina.Remmina -y
 
 # VS Code
-curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > ~/Downloads/vscode.deb
-sudo apt install ~/Downloads/vscode.deb -y
+curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > ~/vscode.deb
+sudo apt install ~/vscode.deb -y
 sudo cp /usr/share/applications/code.desktop /usr/share/applications/code.desktop.orig
 sudo sed -i 's/Exec=\/usr\/share\/code\/code --unity-launch %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--unity-launch %F"/g' /usr/share/applications/code.desktop
 sudo sed -i 's/Exec=\/usr\/share\/code\/code --new-window %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--new-window %F"/g' /usr/share/applications/code.desktop
