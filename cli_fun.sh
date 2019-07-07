ed43f919cf1894a3ab9b97b3c12f8a77f06c19ce
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-07-07 19:04:15 +1000

    Create cli_fun.sh

---
+++ cli_fun.sh
@@ -0,0 +1,12 @@
+# The aim of this file was to learn a bit more about bash in the process of automating more of what I do from the CLI.
+
+# URL redirect
+#
+# Say I want to open a particular website, Splunk for instance. I might want to open Splunk on my local, test or prod server
+# it would be nice if I could type 's' then a particular query which would then redirect me in my browser to said URL.
+
+
+# grep
+#
+# Quite simply, I see too many people around me using grep and I've really not spent any time learning how to use it minus
+# the occassional command I'll copy and paste.
