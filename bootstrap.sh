5eb73d8f6c969e01c347a7194d24bd7cdce17ceb
Chris Franco <11533365+Francommit@users.noreply.github.com>
2021-04-30 19:40:44 +1000

    Removed Tilix Google terminal is better

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,16 +12,6 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt-get update
 sudo apt-get install sublime-text
 
-# Tilix (Terminal alternative)
-echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee /etc/apt/sources.list.d/stretch-backports.list
-sudo apt update
-sudo apt -y install tilix
-sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
-# Add to path
-if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
-    source /etc/profile.d/vte.sh
-fi
-
 # VSCode Installation
 curl -L "https://go.microsoft.com/fwlink/?LinkID=760868" > vscode.deb
 sudo apt install ./vscode.deb -y
